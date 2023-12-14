a93f36d654e2dab417c644dcc35e05f067c5f896
unknown <matteo.licciardino@istitutoagnelli.it>
2023-12-14 15:32:34 +0100

    Fatto la pubblicazione di todos-web

---
+++ docs/svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	adapter: adapter({
+		pages: 'docs',
+		assets: 'docs',
+		fallback: undefined,
+		precompress: false,
+		strict: true
+	})
+};
+
+export default config;
